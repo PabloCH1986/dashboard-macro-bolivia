8dc8c66e7a8269549fca6589cdd19562e4152ff0
PabloStylo <pcachaga@gmail.com>
2026-06-17 18:26:25 -0400

    Update app.py

--- app.py
+++ app.py
@@ -305,16 +305,16 @@ def kpi(df, titulo, col, unidad="", tipo="ultimo", delta_tipo="interanual"):
     delta = None
 
     if delta_tipo == "interanual":
-    yoy = variacion_interanual(df, col)
-    delta = f"{formato_numero_1d(yoy)}% interanual" if yoy is not None else None
+        yoy = variacion_interanual(df, col)
+        delta = f"{formato_numero_1d(yoy)}% interanual" if yoy is not None else None
 
     elif delta_tipo == "pp":
-    pp = variacion_interanual_pp(df, col)
-    delta = f"{formato_numero_1d(pp)} p.p. interanual" if pp is not None else None
+        pp = variacion_interanual_pp(df, col)
+        delta = f"{formato_numero_1d(pp)} p.p. interanual" if pp is not None else None
 
     elif delta_tipo == "acumulado_pct":
-    yoy_acum = variacion_acumulada_interanual(df, col)
-    delta = f"{formato_numero_1d(yoy_acum)}% acum. interanual" if yoy_acum is not None else None
+        yoy_acum = variacion_acumulada_interanual(df, col)
+        delta = f"{formato_numero_1d(yoy_acum)}% acum. interanual" if yoy_acum is not None else None
 
     elif delta_tipo == "ninguno":
         delta = None
